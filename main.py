c7c68653ba38c2f6c6fd5559725dcc89ce5d59cb
diamondBelema <diamondbelema78@gmail.com>
2025-05-21 19:26:19 +0100

    Add CORS middleware and update dependencies
    Configured FastAPI to include CORS middleware for handling cross-origin requests. Updated `requirements.txt` to add dependencies for Pydantic, Requests, and Starlette. These changes enhance compatibility and support for the FastAPI application.

--- main.py
+++ main.py
@@ -3,18 +3,33 @@ from fastapi import FastAPI
 from app.core.database import create_db_and_tables
 from app.routes.admin_routes import admin_router
 from app.routes.user_routes import users_router
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def lifespan(_: FastAPI):
     create_db_and_tables()
     yield
+
+origins = [
+    "http://0.0.0.0:8000",
+    "https://your-production-domain.com"  # production domain
+]
 
 app = FastAPI(
     lifespan=lifespan,
     title = "NVC API",
     description = "API for NVC",
-    version = "0.0.1",
+    version="0.0.1"
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 app.include_router(users_router, prefix="/users")
 app.include_router(admin_router, prefix="/admin")
 
