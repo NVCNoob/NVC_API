c5f618038f002b80f4a846dcfc5120e237c7064d
diamondBelema <diamondbelema78@gmail.com>
2025-05-21 10:40:37 +0100

    Add new imports for User and UserLoginRead in user routes

--- app/routes/admin_routes.py
+++ app/routes/admin_routes.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlmodel import Session
 from uuid import uuid4
-
 from app.core.database import get_db
 from app.models.api_key_models import APIKeyCreate, APIKeyRead
 from app.schemas.api_key import APIKey
@@ -10,7 +9,7 @@ from app.services.api_key_service import admin_only
 admin_router = APIRouter()
 
 @admin_router.post("/create-api-key")
-async def create_api_key(api_key: APIKeyCreate, db: Session = Depends(get_db), _=Depends(admin_only)):
+async def create_api_key(api_key: APIKeyCreate, db: Session = Depends(get_db), _=Depends(admin_only)) -> APIKeyRead:
     key_str = str(uuid4())
 
     api_key = APIKey(
