ceead15398b07ed3db74e025dcbdbc1c56d5ff19
NVCNoob <162205235+NVCNoob@users.noreply.github.com>
2025-06-14 14:44:05 +0000

    fixed the api key not being a pydantic model error

--- app/routes/admin_routes.py
+++ app/routes/admin_routes.py
@@ -5,21 +5,23 @@ from app.core.database import get_db
 from app.models.api_key_models import APIKeyCreate, APIKeyRead
 from app.schemas.api_key import APIKey
 from app.services.api_key_service import admin_only
+from datetime import datetime, timezone
 
 admin_router = APIRouter()
 
 @admin_router.post("/create-api-key")
-async def create_api_key(api_key: APIKeyCreate, db: Session = Depends(get_db), _=Depends(admin_only)) -> APIKeyRead:
+async def create_api_key(api_key_data: APIKeyCreate, db: Session = Depends(get_db), _=Depends(admin_only)) -> APIKeyRead:
     key_str = str(uuid4())
 
-    new_api_key = APIKey(
+    api_key = APIKey(
         key=key_str,
-        name=api_key.name,
-        is_active=True
+        name=api_key_data.name,
+        is_active=True,
+        created_at = datetime.now(timezone.utc)
     )
 
     db.add(api_key)
     db.commit()
     db.refresh(api_key)
 
-    return APIKeyRead(api_key=new_api_key.key, name=new_api_key.name)
+    return APIKeyRead(api_key=api_key.key, name=api_key.name)
